681306da1d764c30109e9b165214ae07ec07d1c1
Diego Rufino <diegorufino1@gmail.com>
2021-09-30 22:28:11 -0300

    Inserindo useState com Context API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Profile from "./components/profile";
 import { AuthContext } from "./providers/auth";
 
 function App() {
-  const { user } = React.useContext(AuthContext);
-  console.log(user);
+  const { setUser } = React.useContext(AuthContext);
+
   return (
     <div className="App">
-      <h1>Nome: {user.name}</h1>
+      <h1>Olá</h1>
+      <input type="text" onChange={(e) => setUser({ name: e.target.value })} />
+      <Profile />
     </div>
   );
 }
--- src/providers/auth.js
+++ src/providers/auth.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AuthContext = React.createContext({});
 
 export const AuthProvider = (props) => {
-  const user = {
-    name: "Diego Rufino",
-  };
+  const [user, setUser] = useState({
+    name: "Diego",
+  });
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {props.children}
     </AuthContext.Provider>
   );
